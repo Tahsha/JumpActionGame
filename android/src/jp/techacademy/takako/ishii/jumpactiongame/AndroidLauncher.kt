f7ded6b3cb4fa417a16452a355637dc737654b7d
takakoi <lifedesign.chris@gmail.com>
2020-12-20 00:16:22 +0900

    error

--- android/src/jp/techacademy/takako/ishii/jumpactiongame/AndroidLauncher.kt
+++ android/src/jp/techacademy/takako/ishii/jumpactiongame/AndroidLauncher.kt
@@ -1,10 +1,11 @@
 package jp.techacademy.takako.ishii.jumpactiongame
 
+
 import android.os.Bundle
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import jp.techacademy.takako.ishii.jumpactiongame.JumpActionGame
+import jp.techacademy.taro.kirameki.jumpactiongame.JumpActionGame
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
